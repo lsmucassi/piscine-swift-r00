ac774150126c9191fdde2ec013b7cfddce97cbec
Siphiwe DLAMINI <sidlamin@e4r4p5.wethinkcode.co.za>
2018-10-06 15:08:56 +0200

    Do: UI most pages

--- Rush00/Rush00/ViewController.swift
+++ Rush00/Rush00/ViewController.swift
@@ -19,7 +19,7 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-}
+    
     override func viewDidAppear(_ animated: Bool) {
         self.performSegue(withIdentifier: "loginView", sender: self);
     }
