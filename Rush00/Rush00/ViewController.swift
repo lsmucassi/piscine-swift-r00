ffecf2e81ae5e425e2278006b486bc1a2fbcb302
Siphiwe DLAMINI <sidlamin@e4r4p5.wethinkcode.co.za>
2018-10-06 11:30:28 +0200

    Do: protected view login

---
+++ Rush00/Rush00/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Rush00
+//
+//  Created by Siphiwe DLAMINI on 2018/10/06.
+//  Copyright © 2018 Siphiwe DLAMINI. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        self.performSegue(withIdentifier: "loginView", sender: self);
+    }
+}
